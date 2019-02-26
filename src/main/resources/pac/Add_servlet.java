f931b557d53ccd1567c1eb004ab64cfd5c90bee6
lostre <lostre@lostre-PC>
2019-02-26 19:52:56 +0200

    web xml

---
+++ src/main/resources/pac/Add_servlet.java
@@ -0,0 +1,11 @@
+package pac.add;
+
+public class Add_servlet {
+
+	public void service(HttpServiceRequest req, HttpServiceReposnce res) {
+		int i =Integer.parseInt( HttpServiceRequest("num1");
+		int j =Integer.parseInt( HttpServiceRequest("num2");
+		int k = i+j;
+		System.out.prinln(k);
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,5 +6,12 @@
 
 <%= new java.util.Date() %>
 
+<form action="add" method="get">
+  first num: <input type="text" name="num1"><br>
+  second num: <input type="text" name="num2"><br>
+  <input type="submit" value="Submit">
+</form>
+
+	
 </body>
 </html>
